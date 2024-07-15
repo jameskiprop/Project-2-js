374dff7a1bd3b3be2a56d5d44f48ca0ac8fe1de0
jameskiprop <jameskiprop01@gmail.com>
2024-07-15 18:19:45 +0300

    completed the first part of the project

---
+++ script.js
@@ -0,0 +1,40 @@
+// String manipulation tactics
+
+/* 1. Reverse a String: Write a function that reverses a given string.*/
+
+function reverseString(str) {
+  //using the split() method to return a new array
+  let splitString = str.split("");
+  console.log(splitString);
+
+  // using the reverse method to reverse the newly created array
+  let reverseArray = splitString.reverse();
+  console.log(reverseArray);
+  //using the join () method to join all elements of the array into a string
+  let joinArray = reverseArray.join("");
+
+  return joinArray;
+}
+
+let result = reverseString("jamie");
+console.log(result);
+
+/* 2. Count Characters: Create a function that counts the number of characters in a string.*/
+
+/* 3. Capitalize Words: Implement a function that capitalizes the first letter of each word in a sentence */
+
+//Array Functions
+
+/* 1. Find Maximum and Minimum: Write functions to find the maximum and minimum values in an array of numbers.*/
+
+/* 2. Sum of Array: Create a function that calculates the sum of all elements in an array.*/
+
+/* 3. Filter Array: Implement a function that filters out elements from an array based on a given condition.*/
+
+//Mathematical Functions
+
+/* 1. Factorial: Write a function to calculate the factorial of a given number.*/
+
+/* 2. Prime Number Check: Create a function to check if a number is prime or not.*/
+
+/* 3. Fibonacci Sequence: Implement a function to generate the Fibonacci sequence up to a given number of terms. (search on the net )*/
