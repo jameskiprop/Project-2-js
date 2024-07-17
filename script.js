6642b8c85690c31ce25167a8cc28fa4e5895d288
jameskiprop <jameskiprop01@gmail.com>
2024-07-17 12:23:07 +0300

    capitalizing the first letter in a function

--- script.js
+++ script.js
@@ -1,40 +1,49 @@
-// String manipulation tactics
+// // String manipulation tactics
 
-/* 1. Reverse a String: Write a function that reverses a given string.*/
+// /* 1. Reverse a String: Write a function that reverses a given string.*/
 
-function reverseString(str) {
-  /*using the split() method to return a new array*/
-  let splitString = str.split("");
-  console.log(splitString);
+// function reverseString(str) {
+//   /*using the split() method to return a new array*/
+//   let splitString = str.split("");
+//   console.log(splitString);
 
-  /*using the reverse method to reverse the newly created array*/
-  let reverseArray = splitString.reverse();
-  console.log(reverseArray);
-  /*using the join () method to join all elements of the array into a string*/
-  let joinArray = reverseArray.join("");
+//   /*using the reverse method to reverse the newly created array*/
+//   let reverseArray = splitString.reverse();
+//   console.log(reverseArray);
+//   /*using the join () method to join all elements of the array into a string*/
+//   let joinArray = reverseArray.join("");
 
-  return joinArray;
-}
-let result = reverseString("jamie");
-console.log(result);
+//   return joinArray;
+// }
+// let result = reverseString("jamie");
+// console.log(result);
 
-/* 2. Count Characters: Create a function that counts the number of characters in a string.*/
+// /* 2. Count Characters: Create a function that counts the number of characters in a string.*/
 
-function characterCount(str) {
-  let count = 0; /*initializing the first count position to be zero*/
-  // iterating through every character in the string
-  for (let i = 0; i < str.length; i++) {
-    count++;
-  }
-  return count;
-}
-let result = characterCount("Kiprop is a FullStack Developer");
-console.log(result);
+// function characterCount(str) {
+//   let count = 0; /*initializing the first count position to be zero*/
+//   // iterating through every character in the string
+//   for (let i = 0; i < str.length; i++) {
+//     count++;
+//   }
+//   return count;
+// }
+// let result = characterCount("Kiprop is a FullStack Developer");
+// console.log(result);
 
 /* 3. Capitalize Words: Implement a function that capitalizes the first letter of each word in a sentence */
+//defining the function
+function capitalizeFirstLetters(sentence) {
+  //splitting the sentence into separate words
+  let sentence = "Kiprop is a full stack developer";
+  let words = sentence.split("");
+  //Capitalizing the first letter of each word
+  const modStr = str[(0, 6, 9, 13, 18)].toUppercase() + str.slice(1);
+  //Joining the words back into a sentence
+  //Returning the result
 
-//Array Functions
-
+  //Array Functions
+}
 /* 1. Find Maximum and Minimum: Write functions to find the maximum and minimum values in an array of numbers.*/
 
 /* 2. Sum of Array: Create a function that calculates the sum of all elements in an array.*/
